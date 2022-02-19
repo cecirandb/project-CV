88786863a2caec74b1285a122954f6a171146a00
Cecilia Cuello <cecicuellorandb@hotmail.com>
2022-02-19 12:59:57 -0300

    initial commit

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutMe.css'
+
+function AboutMe() {
+  return (
+    <div className='about'>
+        <div className='name'>
+            <h1>Cecilia Cuello</h1>
+            <p>description</p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutMe;
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+        <div>Mis Proyectos</div>
+        <div>1</div>
+        <div>2</div>
+    </div>
+  )
+}
+
+export default Projects;
